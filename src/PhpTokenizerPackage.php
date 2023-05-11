27da1713a53d72abc5e1fe61df5693fc9f789057
Patrick Morpey <patrick@morphp.nl>
2023-05-11 10:57:45 +0200

    Replaced placeholders

--- src/PhpTokenizerPackage.php
+++ src/PhpTokenizerPackage.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Medas\Placeholder;
+namespace Medas\PhpTokenizer;
 
 use Medas\Core\AsSingleton;
 use Medas\ServiceManager\BasePackage;
 
-class PlaceholderPackage extends BasePackage
+class PhpTokenizerPackage extends BasePackage
 {
     use AsSingleton;
 
