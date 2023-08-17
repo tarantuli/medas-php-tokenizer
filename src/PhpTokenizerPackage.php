b45f6dd1041f4f632ed8405e5725cab0cf522d15
Patrick Morpey <patrick@morphp.nl>
2023-08-17 09:09:11 +0200

    Updated dependencies

--- src/PhpTokenizerPackage.php
+++ src/PhpTokenizerPackage.php
@@ -13,8 +13,7 @@ class PhpTokenizerPackage extends BasePackage
 
     public function dependencies(): array
     {
-        return $this->dependenciesByClass([
-        ]);
+        return [];
     }
 
     public function sourceDirectory(): string
