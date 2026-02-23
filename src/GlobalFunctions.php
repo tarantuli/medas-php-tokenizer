5d96db5fb8198374df666d151aea55d1f2ed1ef8
Patrick Morpey <patrick@morphp.nl>
2026-02-23 11:36:46 +0100

    Added function defining guard

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 use Medas\PhpTokenizer\{Block, BlockDumper, Statement};
 
 // This file should be in the global namespace
-function dumpBlock(Block $block): void
-{
-    service(BlockDumper::class)->dump($block);
-}
+if (!function_exists('dumpBlock')) {
+    function dumpBlock(Block $block): void
+    {
+        service(BlockDumper::class)->dump($block);
+    }
 
-function dumpStatement(Statement $statement): void
-{
-    service(BlockDumper::class)->dumpStatement($statement);
+    function dumpStatement(Statement $statement): void
+    {
+        service(BlockDumper::class)->dumpStatement($statement);
+    }
 }
