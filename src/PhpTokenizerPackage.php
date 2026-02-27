5c3e9d1cd502c2d07a7e73380eea6a180ae89bd5
Patrick Morpey <patrick@morphp.nl>
2026-02-27 13:37:25 +0100

    Updated dependencies

--- src/PhpTokenizerPackage.php
+++ src/PhpTokenizerPackage.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\PhpTokenizer;
 
-use Medas\Core\AsSingleton;
-use Medas\ServiceManager\{BasePackage, ServiceConfig};
+use Medas\Core\{AsSingleton, BasePackage, Interfaces\ServiceConfig};
 
 class PhpTokenizerPackage extends BasePackage
 {
